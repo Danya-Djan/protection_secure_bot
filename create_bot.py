d96b18996648ece336452de4e3124b73891240b6
Danya-Djan <74513497+Danya-Djan@users.noreply.github.com>
2023-12-11 11:45:57 +0300

    Upload update.2

--- create_bot.py
+++ create_bot.py
@@ -2,6 +2,6 @@ from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
-bot = Bot(token="6959141269:AAGqD8uu5R5owcs2745B7mgc3_5wMDwAGlE")
+bot = Bot(token="TOKEN")
 
 user_list = ['danyadjan', 'Arscool', 'volkov1vl', 'kartamonov', 'Reliable_Princess', 's1lur']
